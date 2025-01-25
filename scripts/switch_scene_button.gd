5dcb76766fa361195ba27f5cb4191a654fde0bc6
Wes-Ray <wesleyray95@gmail.com>
2025-01-25 14:42:43 -0600

    updated trench for level 2

--- scripts/switch_scene_button.gd
+++ scripts/switch_scene_button.gd
@@ -4,7 +4,14 @@ class_name SwitchSceneButton
 # @export var scene_select : PackedScene
 @export_file("*.tscn") var scene_select: String
 
+# func _on_button_up():
+# 	assert(scene_select, "scene must be added in editor, found: '%s'" % scene_select)
+# 	print("changing scene to: ", scene_select)
+# 	get_tree().change_scene_to_file(scene_select)
+
 func _on_button_up():
-	assert(scene_select, "scene must be added in editor, found: %s" % scene_select)
-	print("changing scene to: ", scene_select)
-	get_tree().change_scene_to_file(scene_select)
+    if not scene_select.is_empty():
+        print("changing scene to: ", scene_select)
+        get_tree().change_scene_to_file(scene_select) 
+    else:
+        print("No scene selected")
