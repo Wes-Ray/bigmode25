0defbf931a89fd7eab1e427f6f153e808a7abcba
Brian Guerrero <BGuerrero825@gmail.com>
2025-01-24 22:58:25 -0600

    basic blast door with shield blink and closing door animation

--- deploy_to_server.ps1
+++ deploy_to_server.ps1
@@ -6,8 +6,11 @@ if (-not (Test-Path "deployment.json")) {
 }
 $config = Get-Content -Path "deployment.json" | ConvertFrom-Json
 $GODOT_PATH = $config.godot_path
+Write-Host $GODOT_PATH
 $PROJECT_PATH = $config.project_path
+Write-Host $PROJECT_PATH
 $BUILD_PATH = $config.build_path
+Write-Host $BUILD_PATH
 
 $REMOTE_SERVER = "blog"  # key config must be setup in .ssh
 
@@ -36,7 +39,7 @@ foreach ($path in $paths) {
 # Build the web export
 Write-Host "[*] Building web export..."
 
-$buildProcess = Start-Process -FilePath $GODOT_PATH -ArgumentList "--headless", "--export-release", "Web", "$BUILD_PATH\$PROJECT_NAME.html", $PROJECT_PATH -NoNewWindow -PassThru -Wait
+$buildProcess = Start-Process -FilePath $GODOT_PATH -ArgumentList "--headless", "--export-release", "Web", "`"$BUILD_PATH\$PROJECT_NAME.html`"", $PROJECT_PATH -NoNewWindow -PassThru -Wait
 
 if ($buildProcess.ExitCode -eq 0) {
    Write-Host "[*] Build successful, deploying to server..."
