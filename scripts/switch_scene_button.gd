d34908fa8f756d9d2f2bfccfd2848a09b94d033c
Wes-Ray <wesleyray95@gmail.com>
2025-01-25 12:35:28 -0600

    fixed menus

--- scripts/switch_scene_button.gd
+++ scripts/switch_scene_button.gd
@@ -1,8 +1,10 @@
 extends Control
 class_name SwitchSceneButton
 
-@export var scene_select : PackedScene
+# @export var scene_select : PackedScene
+@export_file("*.tscn") var scene_select: String
 
 func _on_button_up():
-	assert(scene_select, "scene must be added in editor")
-	get_tree().change_scene_to_packed(scene_select)
+	assert(scene_select, "scene must be added in editor, found: %s" % scene_select)
+	print("changing scene to: ", scene_select)
+	get_tree().change_scene_to_file(scene_select)
