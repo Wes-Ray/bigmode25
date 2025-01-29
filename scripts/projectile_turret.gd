875c77ead56f854665046b876561436e985f89e8
DallasHuff <dallashuff45@gmail.com>
2025-01-28 20:51:35 -0600

    new turret model that swivels

--- scripts/projectile_turret.gd
+++ scripts/projectile_turret.gd
@@ -8,20 +8,26 @@ enum firing_type {DIRECT, RANDOM, PREDICT}
 @export_range(0.5, 10) var fire_rate := 2.0
 @export var projectile : PackedScene
 @export_range(50, 500) var projectile_speed := 200.0
+var cannon_skeleton : Skeleton3D
+var cannon_bone_idx : int
 
 @onready var attack_timer : Timer = %AttackTimer
-@onready var top : Node3D = %Top
 @onready var ship : Ship = get_tree().get_first_node_in_group("ship")
-@onready var shooting_point : Node3D = %ShootingPoint
+@onready var shooting_point : Marker3D = %ShootingPoint
+@onready var turret_top : Node3D = %TurretTop
+@onready var direction_finder : Node3D = %DirectionFinder
 
 
 func _ready() -> void:
 	assert(projectile, "projectile scene must be assigned")
 	attack_timer.wait_time = fire_rate
+	cannon_skeleton = turret_top.find_child("Skeleton3D")
+	cannon_bone_idx = cannon_skeleton.find_bone("Bone.001")
+
 
 func _process(_delta: float) -> void:
 	if ship and get_distance(ship.global_position) < attack_range:
-		top.look_at(ship.global_position)
+		swivel()
 		if attack_timer.is_stopped():
 			match fire_mode:
 				firing_type.DIRECT:
@@ -34,6 +40,20 @@ func _process(_delta: float) -> void:
 	else:
 		ship = get_tree().get_first_node_in_group("ship")
 
+
+func swivel() -> void:
+	var dir : Vector3 = ship.global_position - turret_top.global_position
+	var dir_horizontal := Vector3(dir.x, 0, dir.z)
+	if dir_horizontal.length() < 0.001:
+		dir_horizontal = Vector3.FORWARD
+	else:
+		dir_horizontal = dir_horizontal.normalized()
+	var target_horizontal_rot = Basis.looking_at(dir_horizontal, Vector3.UP)
+	turret_top.quaternion = turret_top.quaternion.slerp(target_horizontal_rot.get_rotation_quaternion(), 0.5)
+
+	# var pitch_angle := asin(dir.y)
+	# var target_pitch_rot = Quaternion(Vector3.RIGHT, pitch_angle)
+	# cannon_skeleton.set_bone_pose_rotation(cannon_bone_idx, target_pitch_rot)
 
 func generic_shoot() -> Projectile:
 	var proj : Projectile = projectile.instantiate()
