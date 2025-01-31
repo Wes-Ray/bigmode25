2b9241af7dba64115d4b8fe63e9a7e117c670756
Wes-Ray <wesleyray95@gmail.com>
2025-01-30 21:01:28 -0600

    updated hud and y offset

--- scripts/y_checker.gd
+++ scripts/y_checker.gd
@@ -7,7 +7,7 @@ func _process(_delta: float) -> void:
 	if is_instance_valid(ship):
 		if ship.global_position.y > global_position.y:
 			EventsBus.ceiling_warning_started.emit()
-			if ship.global_position.y > global_position.y + 20.:
+			if ship.global_position.y > global_position.y + 40.:
 				ship.player_crashed.emit()
 		else:
 			EventsBus.ceiling_warning_stopped.emit()
