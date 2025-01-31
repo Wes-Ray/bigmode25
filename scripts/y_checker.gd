12d78635ed4cd134d5319d16ecfced10858ab1ee
DallasHuff <dallashuff45@gmail.com>
2025-01-30 20:50:42 -0600

    height checker and warning implemented

---
+++ scripts/y_checker.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+var ship : Ship
+
+
+func _process(_delta: float) -> void:
+	if is_instance_valid(ship):
+		if ship.global_position.y > global_position.y:
+			EventsBus.ceiling_warning_started.emit()
+			if ship.global_position.y > global_position.y + 20.:
+				ship.player_crashed.emit()
+		else:
+			EventsBus.ceiling_warning_stopped.emit()
+	else:
+		ship = get_tree().get_first_node_in_group("ship")
