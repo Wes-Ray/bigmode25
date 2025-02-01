ce2e7aa06993fd3a167cce8bbd533b5ad1bbf2ad
Wes-Ray <wesleyray95@gmail.com>
2025-02-01 14:46:02 -0600

    setup basic objective with opening gates

--- scripts/objective_door.gd
+++ scripts/objective_door.gd
@@ -3,12 +3,6 @@ class_name ObjectiveDoor
 
 @export var anim_speed := 1.0
 
-func _process(_delta):
-	if Input.is_action_just_pressed("debug4"):
-		close_objective_door()
-
 func close_objective_door() -> void:
-	print("closing obj door")
 	$AnimationPlayer.speed_scale = anim_speed
 	$AnimationPlayer.play("shut_door")
-	
